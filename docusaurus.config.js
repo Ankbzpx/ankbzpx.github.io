aa5468d7ce7df30bd5efd4e4c9b7a6dc69c519d2
Ruichen Zheng <ankbzpx@hotmail.com>
2023-11-07 17:00:58 +0800

    adjust color palette

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Ruichen Zheng',
+  tagline: 'Ruichen Zheng\'s hideout',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -42,10 +42,6 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
         theme: {
@@ -61,7 +57,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Ruichen Zheng',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
