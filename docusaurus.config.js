481264ae2d7a916e69beed9fe1139318ff88eb35
Ruichen Zheng <ankbzpx@hotmail.com>
2023-11-15 21:16:34 +0800

    update deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,19 +11,20 @@ import rehypeKatex from 'rehype-katex';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Ruichen Zheng',
-  tagline: 'Ruichen Zheng\'s hideout',
+  tagline: 'Ruichen Zheng\'s homepage',
   favicon: 'img/ank.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ankbzpx.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ankbzpx', // Usually your GitHub org/user name.
+  projectName: 'ankbzpx.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
