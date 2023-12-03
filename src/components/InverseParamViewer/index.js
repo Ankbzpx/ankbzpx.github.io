4d32816b53224df93247ed2d8a114a353ea1c382
Ruichen Zheng <ankbzpx@hotmail.com>
2023-12-03 12:50:38 +0800

    use dual contouring for inverse param

--- src/components/InverseParamViewer/index.js
+++ src/components/InverseParamViewer/index.js
@@ -2,12 +2,28 @@ import { Canvas, useLoader } from '@react-three/fiber'
 import React, { useEffect, useMemo, useRef } from 'react'
 import { OrbitControls, Environment } from '@react-three/drei'
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { Color, MeshPhysicalMaterial, LineBasicMaterial, WireframeGeometry, LineSegments } from 'three';
+import { Color, MeshPhysicalMaterial, LineBasicMaterial, WireframeGeometry, LineSegments, BufferAttribute } from 'three';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import SaveFigure from "@site/src/components/SaveFigure";
 
-function Model({ modelPath, color, wireframeColor, wireframe }) {
+function Model({ modelPath, color }) {
+  const model = useLoader(OBJLoader, modelPath, () => { });
+  const modelRef = useRef();
+
+  const copiedModel = useMemo(() => {
+    model.traverse(obj => {
+      if (obj.isMesh) {
+        obj.material = new MeshPhysicalMaterial({ color: new Color(color), roughness: 0.8, clearcoat: 0.2, depthTest: true })
+      }
+    })
+    return model.clone()
+  }, [model])
+
+  return <primitive ref={modelRef} object={copiedModel} />;
+}
+
+function ModelWF({ modelPath, color, wireframeColor, wireframe }) {
   const model = useLoader(OBJLoader, modelPath, () => { });
   const modelRef = useRef();
 
@@ -25,7 +41,7 @@ function Model({ modelPath, color, wireframeColor, wireframe }) {
         obj.material = new MeshPhysicalMaterial({ color: new Color(color), roughness: 0.8, clearcoat: 0.2, depthTest: true })
 
         const wireframeGeo = new WireframeGeometry(obj.geometry)
-        const wireframeMat = new LineBasicMaterial({ color: new Color(wireframeColor), depthTest: true })
+        const wireframeMat = new LineBasicMaterial({ color: new Color(wireframeColor), depthTest: true, linewidth: 1.5 })
         const wireframeMesh = new LineSegments(wireframeGeo, wireframeMat)
 
         wireframeMesh.visible = false
@@ -39,12 +55,33 @@ function Model({ modelPath, color, wireframeColor, wireframe }) {
   return <primitive ref={modelRef} object={copiedModel} />;
 }
 
+function QuadWireframe({ name, color }) {
+  const geoRef = useRef()
+
+  // Maybe faster if preallocate `bufferGeometry` then update, but don't bother for now...
+  useEffect(() => {
+    const data = require(`/mesh/inv_param/${name}.json`)
+    const positions = new Float32Array(data.V)
+    geoRef.current.setIndex(data.uE)
+    geoRef.current.setAttribute('position', new BufferAttribute(positions, 3));
+
+  }, [name])
+
+  return <lineSegments >
+    <bufferGeometry ref={geoRef} >
+    </bufferGeometry>
+    <lineBasicMaterial color={color} linewidth={1.5} />
+  </lineSegments>;
+}
+
 export default function InverseParamViewer({ name, target, position }) {
+  const [useDC, setUseDC] = React.useState(true);
   const [enableWireframe, setEnableWireframe] = React.useState(true);
   const [showSave, setShowSave] = React.useState(false);
 
   return (
     <div>
+      <FormControlLabel control={<Checkbox defaultChecked value={useDC} onChange={() => { setUseDC(!useDC) }} />} label="Dual Contouring" />
       <FormControlLabel control={<Checkbox defaultChecked value={enableWireframe} onChange={() => { setEnableWireframe(!enableWireframe) }} />} label="Enable wireframe" />
       {/* <FormControlLabel control={<Checkbox value={showSave} onChange={() => { setShowSave(!showSave) }} />} label="Show save" /> */}
       <div class="row">
@@ -53,8 +90,15 @@ export default function InverseParamViewer({ name, target, position }) {
             <Canvas camera={{ position: position, fov: 55 }}>
               <Environment files='/img/kiara_1_dawn_1k.hdr' />
               <OrbitControls dampingFactor={0.1} target={target} />
-              <Model modelPath={`/mesh/inv_param/${name}_inv_param_mc.obj`} color='#FF9A2D' wireframeColor='#884700' wireframe={enableWireframe} />
-              {showSave && <SaveFigure name={`${name}_inv_param`} />}
+              {
+                useDC ? <>
+                  <Model modelPath={`/mesh/inv_param/${name}_inv_param_dc.obj`} color='#FF9A2D' />
+                  {enableWireframe && <QuadWireframe name={`${name}_inv_param_dc`} color='#884700' />}
+                </> : <>
+                  <ModelWF modelPath={`/mesh/inv_param/${name}_inv_param_mc.obj`} color='#FF9A2D' wireframeColor='#884700' wireframe={enableWireframe} />
+                </>
+              }
+              {showSave && <SaveFigure name={`${name}_param`} />}
             </Canvas>
             <p style={{ textAlign: 'center' }}>MC in parameterization space</p>
           </div>
@@ -64,8 +108,15 @@ export default function InverseParamViewer({ name, target, position }) {
             <Canvas camera={{ position: position, fov: 55 }}>
               <Environment files='/img/kiara_1_dawn_1k.hdr' />
               <OrbitControls dampingFactor={0.1} target={target} />
-              <Model modelPath={`/mesh/inv_param/${name}_mc.obj`} color='#2DC9FF' wireframeColor='#00526F' wireframe={enableWireframe} />
-              {showSave && <SaveFigure name={`${name}_mc`} />}
+              {
+                useDC ? <>
+                  <Model modelPath={`/mesh/inv_param/${name}_dc.obj`} color='#2DC9FF' />
+                  {enableWireframe && <QuadWireframe name={`${name}_dc`} color='#00526F' />}
+                </> : <>
+                  <ModelWF modelPath={`/mesh/inv_param/${name}_mc.obj`} color='#2DC9FF' wireframeColor='#00526F' wireframe={enableWireframe} />
+                </>
+              }
+              {showSave && <SaveFigure name={`${name}_original`} />}
             </Canvas>
             <p style={{ textAlign: 'center' }} >MC in original space</p>
           </div>
